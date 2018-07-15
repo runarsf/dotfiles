b1685d5eca91384434ffed618f00f3bcee6c4858
Runar <runarsf@protonmail.com>
2018-07-15 13:47:47 +0200

    Update loadtheme

--- bin/loadtheme.sh
+++ bin/loadtheme.sh
@@ -5,6 +5,7 @@ case $1 in
 		echo "Applies color scheme to urxvt config (~/.xres/urxvt)."
 		echo "This requires you to include the ~/.xres/urxvt file in your urxvt config."
 		echo "The urxvt config can usually be found at ~/.Xresources or ~/.Xdefaults."
+		echo "The default theme directory is ~/.xres/themes"
 		echo ""
 		echo "loadtheme [theme]"
 		exit
@@ -15,24 +16,36 @@ case $1 in
 		exit
 		;;
 	-a|--append)
-		if [ ! -f ./$1 ]; then
-			echo "No file with the name $1 found."
-			exit
-		else
+		if [ -f ./$1 ]; then
 			cat ./$1 >> ~/.xres/urxvt
 			xrdb -load ~/.Xresources
-			echo "Appended the file $1 to .Xresources"
+			echo "Appended to ~/.xres/urxvt. [ $1 ]"
+			exit
+		elif [ -f ~/.xres/themes/$1 ]; then
+			cat ~/.xres/themes/$1 >> ~/.xres/urxvt
+			xrdb -load ~/.Xresources
+			echo "Appended to ~/.xres/urxvt. [ $1 ]"
+			exit
+		else
+			echo "No file with the name $1 found."
+			exit
 		fi
 		exit
 		;;
 	*)
-		if [ ! -f ./$1 ]; then
-			echo "No theme with the name $1 found."
-			exit
-		else
+		if [ -f ./$1 ]; then
 			cat ./$1 > ~/.xres/urxvt
 			xrdb -load ~/.Xresources
 			echo "Color scheme applied. [ $1 ]"
+			exit
+		elif [ -f ~/.xres/themes/$1 ]; then
+			cat ~/.xres/themes/$1 > ~/.xres/urxvt
+			xrdb -load ~/.Xresources
+			echo "Color scheme applied. [ $1 ]"
+			exit
+		else
+			echo "No theme with the name $1 found."
+			exit
 		fi
 		exit
 		;;
