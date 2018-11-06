6b000a43dafb0ab1f244851a5143f00a866bdc8f
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-11-06 15:48:32 +0100

    last.sh

--- bin/polybar.sh
+++ bin/polybar.sh
@@ -7,12 +7,11 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-DISPLAY1="$(xrandr -q | grep $(sed '1!d' $HOME/.monitor) | cut -d ' ' -f1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar $1 &
-
 DISPLAY2="$(xrandr -q | grep $(sed '2!d' $HOME/.monitor) | cut -d ' ' -f1)"
 [[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar $1 &
 
+DISPLAY1="$(xrandr -q | grep $(sed '1!d' $HOME/.monitor) | cut -d ' ' -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar $1 &
 
 DISPLAY3="$(xrandr -q | grep $(sed '3!d' $HOME/.monitor) | cut -d ' ' -f1)"
 [[ ! -z $DISPLAY3 ]] && MONITOR=$DISPLAY3 polybar $1 &
