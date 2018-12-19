878148e722c0c289d15ac2370832ac1282327131
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2018-12-19 15:24:01 +0100

    not an empty dir

--- deploy.sh
+++ deploy.sh
@@ -197,7 +197,7 @@ function configs {
 		mkdir ~/.fonts
 	fi
 	git clone https://github.com/runarsf/fonts ~/.fonts/
-	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	vim +PluginInstall +qall
 	done
