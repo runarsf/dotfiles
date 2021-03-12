d960b6d443a25a876dd80b1f92b7da2143a9efac
Runar Fredagsvik <runarsf@protonmail.com>
2021-03-12 12:03:55 +0100

    pluginless md preview vim

--- .config/nvim/mdconvert.sh
+++ .config/nvim/mdconvert.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # https://alexb7711.github.io/Markdown-Live-Preview-Without-Vim-Pluggins/
-file=$(readlink -f "${1}")
-dir=$(dirname "${file}")
+# requires pdflatex, texlive-most on arch
+file="$(readlink -f "${1}")"
+dir="$(dirname "${file}")"
 base="${file%.*}"
-shebang=$(sed -n 1p "${file}")
+shebang="$(sed -n 1p "${file}")"
 
 cd "${dir}" || exit
 
 case "${file}" in
-    *.md) pandoc "${file}" -o "${base}".pdf;; 
+    *.md) pandoc -f markdown -t pdf -o "${base}.pdf" "${file}";;
 esac
 
+if ! pgrep "zathura" >/dev/null 2>&1; then
+  zathura "${base}.pdf" &
+  disown
+fi
