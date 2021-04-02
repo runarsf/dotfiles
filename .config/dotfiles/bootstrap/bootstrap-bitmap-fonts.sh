49c1afdb5d5c424c266e66bb48d016ed93a97cd1
Runar Fredagsvik <runarsf@protonmail.com>
2021-04-02 17:54:17 +0200

    Fri-2021-04-02T17:54:17+0200

---
+++ .config/dotfiles/bootstrap/bootstrap-bitmap-fonts.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#sudo ln -s /etc/fonts/conf.avail/10-* /etc/fonts/conf.d
+#sudo ln -s /etc/fonts/conf.avail/70-yes-bitmaps.conf /etc/fonts/conf.d
+sudo rm -rf /etc/fonts/conf.d/70-no-bitmaps.conf
