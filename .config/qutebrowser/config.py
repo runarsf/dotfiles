b908064667ed022ff6a0ba0732fc1a89e3e4e8ca
Runar Fredagsvik <runarsf@protonmail.com>
2021-06-23 12:52:08 +0200

    Wed-2021-06-23T12:52:08+0200

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,11 +3,11 @@
 # Load existing settings made via :set
 config.load_autoconfig()
 
-#import themes.dracula as draw
-#draw.blood(c, {
-#    'spacing': {
-#        'vertical': 6,
-#        'horizontal': 8
-#    }
-#})
-config.source('themes/nord.py')
+import themes.dracula as draw
+draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+#config.source('themes/nord.py')
