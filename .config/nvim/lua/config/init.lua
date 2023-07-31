e9a707435501e0b9c74c4d81c0740dd3d2482ad0
Runar Fredagsvik <runarsf@protonmail.com>
2023-07-31 11:08:00 +0200

    stop checking for plugin updates

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       disabled_plugins = {
