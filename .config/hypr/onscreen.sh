4f587d4a96974fc3486c3d72b96c3d05da9ae97b
Runar Fredagsvik <i@runar.ch>
2023-11-02 18:08:12 +0100

    fix onscreen

--- .config/hypr/onscreen.sh
+++ .config/hypr/onscreen.sh
@@ -16,6 +16,11 @@ y="$(printf '%s\n' "${window}" | jq -r '.at[1]')"
 w="$(printf '%s\n' "${window}" | jq -r '.size[0]')"
 h="$(printf '%s\n' "${window}" | jq -r '.size[1]')"
 
+old_x="${x}"
+old_y="${y}"
+old_w="${w}"
+old_h="${h}"
+
 mon_x="$(printf '%s\n' "${monitor}" | jq -r '.x')"
 mon_y="$(printf '%s\n' "${monitor}" | jq -r '.y')"
 mon_w="$(printf '%s\n' "${monitor}" | jq -r '.width')"
@@ -23,26 +28,23 @@ mon_h="$(printf '%s\n' "${monitor}" | jq -r '.height')"
 
 max_w="$((${mon_w} - 2 * ${padding}))"
 max_h="$((${mon_h} - 2 * ${padding}))"
+
+test "${w}" -gt "${max_w}" && w="${max_w}"
+test "${h}" -gt "${max_h}" && h="${max_h}"
+
+max_x="$((${mon_w} - ${w} - ${padding}))"
+max_y="$((${mon_h} - ${h} - ${padding}))"
 min_x="$((${mon_x} + ${padding}))"
 min_y="$((${mon_y} + ${padding}))"
-max_x="$((${mon_w} - ${max_w} - ${padding}))"
-max_y="$((${mon_h} - ${max_h} - ${padding}))"
 
-new_x="${x}"
-new_y="${y}"
-new_w="${w}"
-new_h="${h}"
+test "${x}" -gt "${max_x}" && x="${max_x}"
+test "${y}" -gt "${max_y}" && y="${max_y}"
+test "${x}" -lt "${min_x}" && x="${min_x}"
+test "${y}" -lt "${min_y}" && y="${min_y}"
 
-test "${x}" -lt "${min_x}" && new_x="${min_x}"
-test "${y}" -lt "${min_y}" && new_y="${min_y}"
-test "${x}" -gt "${max_x}" && new_x="${max_x}"
-test "${y}" -gt "${max_y}" && new_y="${max_y}"
-test "${w}" -gt "${max_w}" && new_w="${max_w}"
-test "${h}" -gt "${max_h}" && new_h="${max_h}"
-
-if test "${w}" -ne "${new_w}" -o "${h}" -ne "${new_h}"; then
-  hyprctl dispatch resizewindowpixel exact "${new_w}" "${new_h}",pid:${pid}
+if test "${x}" -ne "${old_x}" -o "${y}" -ne "${old_y}"; then
+  hyprctl dispatch movewindowpixel exact "${x}" "${y}",pid:${pid}
 fi
-if test "${x}" -ne "${new_x}" -o "${y}" -ne "${new_y}"; then
-  hyprctl dispatch movewindowpixel exact "${new_x}" "${new_y}",pid:${pid}
+if test "${w}" -ne "${old_w}" -o "${h}" -ne "${old_h}"; then
+  hyprctl dispatch resizewindowpixel exact "${w}" "${h}",pid:${pid}
 fi
