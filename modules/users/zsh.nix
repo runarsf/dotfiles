96224b6ca52d71f5ddaac6112e35fd13cc99cbca
Runar Fredagsvik <i@runar.ch>
2024-08-30 10:51:47 +0200

    revert lf

--- modules/users/zsh.nix
+++ modules/users/zsh.nix
@@ -97,10 +97,6 @@
       bindkey -M vicmd '^G' per-directory-history-toggle-history
 
       autopair-init
-
-      function lf {
-        cd "$(walk "$@")"
-      }
     '';
     prezto = {
       enable = true;
