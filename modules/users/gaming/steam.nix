352deb5d9dbdfdadeb6a5bcc1640ad1da9f6f018
Thomas Espervik <thoesp@protonmail.com>
2024-11-07 17:48:36 +0100

    steam helper

--- modules/users/gaming/steam.nix
+++ modules/users/gaming/steam.nix
@@ -2,12 +2,29 @@
 
 # Sunshine: https://github.com/LongerHV/nixos-configuration/blob/master/modules/nixos/sunshine.nix
 
+# TODO: https://journix.dev/posts/gaming-on-nixos/
 outputs.lib.mkDesktopModule config "steam" {
   home.packages = with pkgs; [ protontricks winetricks gamescope ];
 
-  nixos.programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
+  nixos.programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+    gamemode = {
+      enable = true;
+      enableRenice = true;
+      settings = {
+        general = {
+          softrealtime = "auto";
+          renice = 10;
+        };
+        custom = {
+          start = "notify-send -a 'Gamemode' 'Optimizations activated'";
+          end = "notify-send -a 'Gamemode' 'Optimizations deactivated'";
+        };
+      };
+    };
   };
 }
