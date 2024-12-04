d7e277737069a68b35927a1c64694f90ed63c1ee
Runar Fredagsvik <i@runar.ch>
2024-12-04 21:20:16 +0100

    add cameractrls gui

--- modules/users/camera.nix
+++ modules/users/camera.nix
@@ -1,7 +1,7 @@
 { config, pkgs, outputs, ... }:
 
 outputs.lib.mkDesktopModule config "camera" {
-  home.packages = with pkgs; [ cameractrls ];
+  home.packages = with pkgs; [ cameractrls cameractrls-gtk4 ];
 
   wayland.windowManager.hyprland.settings.exec-once =
     [ "${pkgs.cameractrls}/bin/cameractrlsd" ];
