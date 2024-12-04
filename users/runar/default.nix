40bba812fafa55b7f2617e629721e4b3a532f3b8
Runar Fredagsvik <i@runar.ch>
2024-12-04 03:03:32 +0100

    use zen stable

--- users/runar/default.nix
+++ users/runar/default.nix
@@ -63,16 +63,13 @@ in outputs.lib.mkFor system hostname {
         "mpv"
         "stremio"
         "camera"
+        "zen"
       ] // {
         stylix = {
           enable = true;
           # TODO Make system wide
           system-wide = false;
           theme = "ayu-dark";
-        };
-        zen = {
-          enable = true;
-          version = "twilight";
         };
         python = {
           enable = true;
