50c5a74a61e8e459ffa526bad999b83a02c57071
Runar Fredagsvik <i@runar.ch>
2025-02-06 12:51:41 +0100

    general changes

--- modules/users/webdav.nix
+++ modules/users/webdav.nix
@@ -1,4 +1,4 @@
-{ config, outputs, ... }:
+{ config, outputs, pkgs, name, ... }:
 
 outputs.lib.mkModule config "webdav" {
   modules.nginx.enable = true;
@@ -10,30 +10,59 @@ outputs.lib.mkModule config "webdav" {
       SupplementaryGroups = [ "shadow" ];
     };
 
+    networking.firewall = {
+      allowedTCPPorts = [ 6060 ];
+      allowedUDPPorts = [ 6060 ];
+    };
+    systemd.services.webdav = {
+      after = [ "network.target" ];
+      description = "rclone webdav server";
+      wantedBy = [ "default.target" ];
+
+      serviceConfig = {
+        ExecStart = builtins.toString (pkgs.writeShellScript "rclone-dav" ''
+          #!/run/current-system/sw/bin/bash
+          set -o errexit
+          set -o nounset
+
+          ${pkgs.rclone}/bin/rclone serve webdav ${config.home.homeDirectory}/data/media/music --addr 0.0.0.0:6060 --user ${name} --pass penis
+        '');
+        Restart = "always";
+      };
+    };
+
+    # services.webdav = {
+    #   enable = true;
+    #   settings = {
+    #     address = "0.0.0.0";
+    #     port = 6060;
+    #     modify = true;
+    #     auth = false;
+    #     directory = "${config.home.homeDirectory}/data/media/music";
+    #   };
+    # };
+
     services.nginx.virtualHosts = outputs.lib.mkIf config.modules.nginx.enable {
       "dav.${config.modules.nginx.domain}" = {
         forceSSL = true;
         sslCertificate = config.modules.nginx.cert;
         sslCertificateKey = config.modules.nginx.key;
         locations."/" = {
+          proxyPass = "http://0.0.0.0:6060";
           root = "${config.home.homeDirectory}/data/media/music";
-          index = "index.html index.htm index.nginx-debian.html";
           extraConfig = ''
             dav_methods PUT DELETE MKCOL COPY MOVE;
             dav_ext_methods PROPFIND OPTIONS;
-            dav_access user:rw group:rw all:rw;
+            dav_access user:rw group:rw all:r;
+
+            autoindex on;
 
             client_max_body_size 0;
             create_full_put_path on;
-            client_body_temp_path /tmp/;
+            client_body_temp_path /tmp/nginx/client-bodies;
 
-            auth_pam  "Password Required";
+            auth_pam "Password Required";
             auth_pam_service_name "nginx";
-
-            # Deny all access unless authenticated
-            satisfy all;
-            allow all;  # This allows all authenticated users
-            deny all;   # This denies all other users
           '';
         };
       };
