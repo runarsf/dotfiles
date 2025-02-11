09757b1f27c0df92872806d40ad04d595d572334
Thomas Espervik <thoesp@protonmail.com>
2025-02-11 11:09:06 +0100

    qmk rework

--- modules/users/development/qmk.nix
+++ modules/users/development/qmk.nix
@@ -8,16 +8,23 @@
 outputs.lib.mkModule config "qmk" {
   home.packages = with pkgs; [
     qmk
+    keymapp
   ];
 
-  nixos.services.udev.packages =
-    let
-      qmk-rules = pkgs.writeTextFile {
-        name = "50-qmk.rules";
-        text = builtins.readFile ./qmk.rules;
-        destination = "/etc/udev/rules.d/50-qmk.rules";
-      };
-    in
-    [ qmk-rules ];
-  nixos.services.udev.extraRules = builtins.readFile ./qmk.rules;
+  nixos = {
+    hardware.keyboard.zsa.enable = true;
+
+    services.udev = {
+      packages =
+        let
+          qmk-rules = pkgs.writeTextFile {
+            name = "50-qmk.rules";
+            text = builtins.readFile ./qmk.rules;
+            destination = "/etc/udev/rules.d/50-qmk.rules";
+          };
+        in
+        [ qmk-rules ];
+      extraRules = builtins.readFile ./qmk.rules;
+    };
+  };
 }
