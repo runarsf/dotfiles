6c584bd80b9516c9f4e3d1c661cb26b9b54d77ce
Thomas Espervik <thoesp@protonmail.com>
2025-09-22 16:59:10 +0200

    qmk fix

--- modules/users/development/qmk.nix
+++ modules/users/development/qmk.nix
@@ -10,18 +10,6 @@ outputs.lib.mkModule config "qmk" {
     keymapp
   ];
 
-  nixos = {
-    hardware.keyboard.zsa.enable = true;
-
-    services.udev = {
-      packages = let
-        qmk-rules = pkgs.writeTextFile {
-          name = "50-qmk.rules";
-          text = builtins.readFile ./qmk.rules;
-          destination = "/etc/udev/rules.d/50-qmk.rules";
-        };
-      in [qmk-rules];
-    };
-  };
+  nixos.hardware.keyboard.zsa.enable = true;
   modules.udev.extraRules = [ ./qmk.rules ];
 }
