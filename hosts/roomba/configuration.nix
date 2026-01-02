2c1ddffd82785974a1472c59c0634b2e5e04848f
Majus-Dev <83781075+Majus-Dev@users.noreply.github.com>
2026-01-02 02:17:52 +0100

    roomba

---
+++ hosts/roomba/configuration.nix
@@ -0,0 +1,22 @@
+_: {
+  imports = [
+    ../../modules/linux/locales/norwegian.nix
+    ../../modules/linux/systemd-boot.nix
+    ../../modules/linux/thunderbolt.nix
+    ../../modules/linux/network.nix
+    ../../modules/linux/firewall.nix
+    ../../modules/linux/printing.nix
+  ];
+
+  services.libinput.enable = true;
+  services.xserver.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  system.stateVersion = "24.05";
+
+  boot.kernelParams = [
+    "modset=1"
+    "fbdev=1"
+  ];
+}
