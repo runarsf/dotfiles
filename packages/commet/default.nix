9b495e3fb0752d5efead1db0b04fcf4c10c99093
Thomas Espervik <thoesp@protonmail.com>
2026-07-15 21:36:17 +0200

    actually assign commet package

--- packages/commet/default.nix
+++ packages/commet/default.nix
@@ -1,10 +1,12 @@
 _: {
-  perSystem = {pkgs, ...}: rec {
-    appId = "chat.commet.commetapp";
-    sha256 = "";
-    bundle = "${pkgs.fetchurl {
-      url = "https://github.com/commetchat/commet/releases/download/v0.4.2+hotfix.2/chat.commet.commetapp.flatpak";
-      inherit sha256;
-    }}";
+  perSystem = {pkgs, ...}: {
+    packages.commet = rec {
+      appId = "chat.commet.commetapp";
+      sha256 = "";
+      bundle = "${pkgs.fetchurl {
+        url = "https://github.com/commetchat/commet/releases/download/v0.4.2+hotfix.2/chat.commet.commetapp.flatpak";
+        inherit sha256;
+      }}";
+    };
   };
 }
