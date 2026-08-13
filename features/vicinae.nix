59122b0a566fe3f4f576fe60f224f34c0718056c
Runar Fredagsvik <i@runar.ch>
2026-08-13 14:58:53 +0200

    add vicinae config

--- features/vicinae.nix
+++ features/vicinae.nix
@@ -38,9 +38,84 @@
           pop_to_root_on_close = true;
           favicon_service = "twenty";
           search_files_in_root = true;
-          providers."@sovereign/vicinae-extension-awww-switcher-0".preferences = {
-            wallpaperPath = "${config.home.homeDirectory}/Pictures/Wallpapers";
-            transitionDuration = 1;
+          activate_on_single_click = true;
+          wrap_navigation = true;
+          escape_key_behavior = "close_window";
+          global_shortcuts.toggle = "";
+          telemetry.system_info = false;
+          launcher_window.compact_mode.enabled = true;
+          providers = {
+            "@luleyleo/vicinae-extension-wiktionary-0" = {
+              preferences = {
+                resultLanguages = "en,de";
+                source = "empty";
+              };
+              entrypoints = {
+                define = {
+                  alias = "dict";
+                };
+              };
+            };
+            "@luolei/karakeep" = {
+              enabled = true;
+              preferences = {
+                apiUrl = "https://links.runar.ch";
+                displayBookmarkPreview = true;
+                displayBookmarkStatus = false;
+                displayCreatedAt = true;
+                displayDescription = true;
+                displayNote = false;
+                displaySummary = false;
+                displayTags = true;
+                language = "en";
+                linkMainAction = "viewDetail";
+                prefillUrlFromBrowser = true;
+                showWebsitePreview = true;
+                textMainAction = "viewDetail";
+                verboseLogging = false;
+              };
+              entrypoints = {
+                backups.enabled = false;
+                createList.enabled = false;
+                createNote.enabled = false;
+                highlights.enabled = false;
+                notes.enabled = false;
+                stats.enabled = false;
+              };
+            };
+            browser-extension.enabled = false;
+            core = {
+              entrypoints = {
+                about.enabled = false;
+                documentation.enabled = false;
+                keybind-settings.enabled = false;
+                list-extensions.enabled = false;
+                manage-fallback.enabled = false;
+                oauth-token-store.enabled = false;
+                open-default-config.enabled = false;
+                prune-memory.enabled = false;
+                refresh-apps.alias = "reload";
+                reload-scripts.enabled = false;
+                report-bug.enabled = false;
+                search-builtin-icons.enabled = false;
+                show-logs.enabled = false;
+                sponsor.enabled = false;
+                store.enabled = false;
+              };
+            };
+            developer.enabled = false;
+            manage-shortcuts.enabled = false;
+            raycast-compat.enabled = false;
+            snippets.enabled = false;
+            system.enabled = false;
+            theme.enabled = false;
+            wm.enabled = false;
+          };
+          providers = {
+            "@sovereign/vicinae-extension-awww-switcher-0".preferences = {
+              wallpaperPath = "${config.home.homeDirectory}/Pictures/Wallpapers";
+              transitionDuration = 1;
+            };
           };
         };
         extensions = with inputs.vicinae-extensions.packages.${system}; [
